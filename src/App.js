ecbdfc480b3438e259ddc85de43ff44cb170fa98
mattmiller1989 <mattmiller0989@gmail.com>
2021-11-06 20:37:43 -0500

    Added filtering for the tasks

--- src/App.js
+++ src/App.js
@@ -16,9 +16,15 @@ const logoUri = `data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg"
 //     />
 //   );
 // }
-
+const FILTER_MAP = {
+    All: () => true,
+    Active: task => !task.completed,
+    Completed: task => task.completed
+};
+const FILTER_NAMES = Object.keys(FILTER_MAP);
 function App(props) {
   const [tasks, setTasks] = useState(props.tasks);
+  const [filter, setFilter] = useState('All');
   //console.log(props.tasks);
 
     function toggleTaskCompleted(id) {
@@ -36,17 +42,39 @@ function App(props) {
         setTasks(remainingTasks);
     }
 
-  const taskList = tasks.map((task) => (
-    <Todo
-      id={task.id}
-      name={task.name}
-      completed={task.completed}
-      key={task.id}
-      toggleTaskCompleted={toggleTaskCompleted}
-      deleteTask={deleteTask}
-    />
-  ));
+    function editTask(id, newName) {
+        const editedTaskList = tasks.map(task => {
+            // if this task has the same ID as the edited task
+            if (id === task.id) {
+                //
+                return {...task, name: newName}
+            }
+            return task;
+        });
+        setTasks(editedTaskList);
+    }
 
+    const taskList = tasks
+        .filter(FILTER_MAP[filter])
+        .map(task => (
+            <Todo
+                id={task.id}
+                name={task.name}
+                completed={task.completed}
+                key={task.id}
+                toggleTaskCompleted={toggleTaskCompleted}
+                deleteTask={deleteTask}
+                editTask={editTask}
+            />
+        ));
+    const filterList = FILTER_NAMES.map(name => (
+        <FilterButton
+            key={name}
+            name={name}
+            isPressed={name === filter}
+            setFilter={setFilter}
+        />
+    ));
   function addReminder(name) {
     const newTask = { id: "todo-" + nanoid(), name: name, completed: false };
     setTasks([...tasks, newTask]);
@@ -60,9 +88,7 @@ function App(props) {
       <h1>TodoMatic</h1>
       <Form addReminder={addReminder} />
       <div className="filters btn-group stack-exception">
-        <FilterButton />
-        <FilterButton />
-        <FilterButton />
+          {filterList}
       </div>
         <h2 id="list-heading">{headingText}</h2>
       <ul
